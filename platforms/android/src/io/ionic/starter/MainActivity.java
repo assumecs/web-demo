76ead43d956effcc5b27ca28dcd2a309cb8fc36b
assumecs <jiuhongxi@sina.com>
2017-08-16 01:41:04 +0800

    permissions request

--- platforms/android/src/io/ionic/starter/MainActivity.java
+++ platforms/android/src/io/ionic/starter/MainActivity.java
@@ -25,8 +25,12 @@ import android.os.Build;
 import android.os.Bundle;
 import org.apache.cordova.*;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class MainActivity extends CordovaActivity
 {
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -37,12 +41,26 @@ public class MainActivity extends CordovaActivity
         if (extras != null && extras.getBoolean("cdvStartInBackground", false)) {
             moveTaskToBack(true);
         }
-        if(Build.VERSION.SDK_INT >= 23 && PackageManager.PERMISSION_DENIED == checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)) {
-            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        if(Build.VERSION.SDK_INT >= 23){
+            ArrayList<String> persList = new ArrayList<String>();
+            if(PackageManager.PERMISSION_DENIED == checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)){
+                persList.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+            }
+            if(PackageManager.PERMISSION_DENIED == checkSelfPermission(Manifest.permission.CAMERA)){
+                persList.add(Manifest.permission.CAMERA);
+            }
+            if(PackageManager.PERMISSION_DENIED == checkSelfPermission(Manifest.permission.RECORD_AUDIO)){
+                persList.add(Manifest.permission.RECORD_AUDIO);
+            }
+            if(persList.size() != 0){
+                String[] permissions = new String[persList.size()];
+                persList.toArray(permissions);
+                requestPermissions(permissions, 1);
+            }
         }
-        if(Build.VERSION.SDK_INT >= 23 && PackageManager.PERMISSION_DENIED == checkSelfPermission(Manifest.permission.CAMERA)) {
-            requestPermissions(new String[]{Manifest.permission.CAMERA}, 2);
-        }
+//        if(Build.VERSION.SDK_INT >= 23 && PackageManager.PERMISSION_DENIED == checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+//            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO}, 1);
+//        }
 
         // Set by <content src="index.html" /> in config.xml
         loadUrl(launchUrl);
